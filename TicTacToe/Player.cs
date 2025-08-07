ad1808a66ed29e9041649f13a9f04af5c42d6148
Ananya Menon <147334173+AnanyaMMenon@users.noreply.github.com>
2025-08-07 14:03:11 -0500

    Updated board, player and game

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace TicTacToe
+{
+    public class Player
+    {
+        public string Name { get; }
+        public char Symbol { get; }
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+    }
+}
